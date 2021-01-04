c9f972ceeb5fefc593d8dfce7d21e4cf9db27689
luismoller <49380630+luismoller@users.noreply.github.com>
2021-01-03 22:37:52 -0800

    html and css pages for planned pages

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask.helpers import url_for
+from flask.templating import render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+if __name__ == "__main__":
+    app.run(debug=1)    
+
+
